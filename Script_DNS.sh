a9b2e2c597df3687c0c1cf3d78e2802468037da9
FrancescMoragues <francescmoragues@paucasesnovescifp.cat>
2026-02-06 17:08:27 +0100

    Update Script_DNS.sh

--- Script_DNS.sh
+++ Script_DNS.sh
@@ -1,13 +1,11 @@
-# Actualizar e instalar bind9 y git
 apt update
 apt upgrade -y
 apt install bind9 git -y
-# Copiar los archivos a la ruta final
+
 cp /tmp/DNS_temp/named.conf.local /etc/bind/
 cp /tmp/DNS_temp/db.erebor.com /etc/bind/
 cp /tmp/DNS_temp/db.inversa /etc/bind/
 
-# Limpiar temporales y reiniciar
 rm -rf /tmp/DNS_temp
 systemctl restart bind9
 systemctl status bind9
