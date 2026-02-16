ca71c30c622ccc0bd4a42f5ddd2559c3cd0de3f3
FrancescMoragues <francescmoragues@paucasesnovescifp.cat>
2026-02-16 18:15:29 +0100

    Update Script_DNS.sh

--- Script_DNS.sh
+++ Script_DNS.sh
@@ -4,9 +4,9 @@ apt update && apt install bind9 git -y
 
 git clone https://github.com/FrancescMoragues/DNS.git /tmp/DNS_temp
 
-cp /tmp/DNS_skoda/named.conf.local /etc/bind/
-cp /tmp/DNS_skoda/db.skoda.com /etc/bind/
-cp /tmp/DNS_skoda/db.10.72.97 /etc/bind/
+cp /tmp/DNS_temp/named.conf.local /etc/bind/
+cp /tmp/DNS_temp/db.skoda.com /etc/bind/
+cp /tmp/DNS_temp/db.10.72.97 /etc/bind/
 
 chown root:bind /etc/bind/named.conf.local /etc/bind/db.skoda.com /etc/bind/db.10.72.97
 chmod 644 /etc/bind/named.conf.local /etc/bind/db.skoda.com /etc/bind/db.10.72.97
